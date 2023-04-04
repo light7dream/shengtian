f11b4bda59391b7817d2b85566f28e65d8df3b67
Fi <ry.mc.le.92@gmail.com>
2023-04-04 11:06:33 -0700

    updated

---
+++ app/Mail/SupportMail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SupportMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($details)
+    {
+        //
+        $this->details=$details;
+    }
+
+    
+
+    public function build()
+    {
+        return $this->subject('Mail from Shengtian')->view('supportmail');
+    }
+}
---
+++ resources/views/supportmail.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{$details['title']}}</title>
+</head>
+<body>
+    <h1>{{$details['from']}}</h1>
+    <p>{{$details['message']}}</p>
+</body>
+</html>
