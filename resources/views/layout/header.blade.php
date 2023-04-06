933a361bf52611b1803cad396654c5334b21927e
Chen Lee <ry.mc.le.92@gmail.com>
2023-04-06 11:36:09 -0700

    updated

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,7 +6,7 @@
     <title>胜天</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="{{asset('assets/favicon.ico')}}" />
+    <link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
     
 	@yield('styles')
 
