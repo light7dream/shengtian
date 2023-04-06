53a6abda918237bedcb0baf53de6fbcf58ef9039
Chen Lee <ry.mc.le.92@gmail.com>
2023-04-06 02:44:25 -0700

    updated

--- database/migrations/2023_03_29_014647_create_online_services_table.php
+++ database/migrations/2023_03_29_014647_create_online_services_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string('description');
+            $table->longText('description');
             $table->timestamps();
         });
     }
