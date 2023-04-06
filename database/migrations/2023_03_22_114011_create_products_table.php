7df329a8d8cef09a063582e7927cfa91387d6494
Chen Lee <ry.mc.le.92@gmail.com>
2023-04-06 11:01:29 -0700

    updated

--- database/migrations/2023_03_22_114011_create_products_table.php
+++ database/migrations/2023_03_22_114011_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->longText('description');
+            $table->binary('description');
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')
               ->references('id')->on('categories')->onDelete('cascade');
--- database/migrations/2023_04_01_191340_create_abouts_table.php
+++ database/migrations/2023_04_01_191340_create_abouts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
-            $table->longText('content');
+            $table->binary('content');
             $table->timestamps();
         });
     }
