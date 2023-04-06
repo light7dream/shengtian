934026aa3d3d3aa9646964bd85bb6d35aaa6e62b
Chen Lee <ry.mc.le.92@gmail.com>
2023-04-06 03:53:33 -0700

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,9 +45,9 @@ class HomeController extends Controller
 
         $best_products = [];
         $products = Product::all();
-        $products = array_values(array_sort($products, function ($value) {
-            return $value->order_products->count();
-        }));
+        // $products = array_values(array_sort($products, function ($value) {
+        //     return $value->order_products->count();
+        // }));
         $best_products = array_slice($products, 8, null, true);
         /**MISSING */
         $new_products = Product::orderBy('created_at', 'desc')->paginate(8);
